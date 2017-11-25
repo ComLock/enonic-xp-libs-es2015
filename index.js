6a16bad7ab973ce9962142abcfe9673a1357beb0
Christian Westgaard <cwe@enonic.com>
2017-11-25 16:40:28 +0100

    Added contentGet and getAttachments

---
+++ index.js
@@ -0,0 +1,27 @@
+const CONTENT_PACKAGE = 'com.enonic.xp.lib.content';
+
+
+export function contentGet({
+  key,
+  branch = null
+}) {
+  return __.toNativeObject(Object.assign(__.newBean(`${CONTENT_PACKAGE}.GetContentHandler`), {
+    branch,
+    key
+  }).execute());
+  /*
+    const bean = __.newBean(`${CONTENT_PACKAGE}.GetContentHandler`);
+    bean.key = key;
+    bean.branch = branch;
+    return __.toNativeObject(bean.execute());
+  */
+}
+
+
+export function getAttachments({
+  key = null
+} = {}) {
+  const bean = __.newBean(`${CONTENT_PACKAGE}.GetAttachmentsHandler`);
+  bean.key = key;
+  return __.toNativeObject(bean.execute());
+}
