83d2f3ab97ff5a9164811b5124b0826b364a106c
Christian Westgaard <cwe@enonic.com>
2017-11-26 17:20:08 +0100

    Forgotten functions

--- double-underscore.js
+++ double-underscore.js
@@ -1,5 +1,21 @@
+export function disposer(func) {
+  return __.disposer(func);
+}
+
+
+// TODO I broke the API here, I probably shouldn't do that.
 export function newBean(packageName, className, params = {}) {
   return { ...__.newBean(`${packageName}.${className}`), params };
+}
+
+
+export function nullOrValue(value) {
+  return __.nullOrValue(value);
+}
+
+
+export function registerMock(name, value) {
+  return __.registerMock(name, value);
 }
 
 
